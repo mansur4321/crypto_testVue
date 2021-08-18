e124bf68dd25dff9806d876a10e5c77246f97243
mansur4321 <jet_011@mail.ru>
2021-08-19 02:36:24 +0300

    Crypto_ajax
    Добавил запросы на сайт курсов криптовалют, беру оттуда их цену(изменил css и добавил событие)

--- cryptonomicon/src/App.vue
+++ cryptonomicon/src/App.vue
@@ -16,10 +16,14 @@
 			<hr style="width: 800px;">
 			<div 
 			v-for="t of tickers"
-			:key="t.name"
-			class="currency">
+			@click="sel = t" 
+			:key="t.name" 
+			class="currency"
+			:class="{
+				'bdColor': sel == t,
+			}">
 				<p class="currency__name">{{t.name}} - USD</p>
-				<p class="currency__price">-</p>
+				<p class="currency__price">{{t.price}}</p>
 				<div class="btn"><a
 				@click="tickerDelete(t.name)"
 				href="#" class="currency__btnDel">Удалить</a></div>
@@ -39,8 +43,9 @@ export default {
 			tickers: [{
 				name: '',
 				price: ''
-			},]
-		}
+			},],
+			sel: null,
+		};
 	},
 
 	methods: {
@@ -51,6 +56,13 @@ export default {
 			}
 
 			this.tickers.push(newTicker);
+
+			setInterval( async () => {
+				const s = await fetch(`https://min-api.cryptocompare.com/data/price?fsym=${newTicker.name}&tsyms=USD&api_key=b4fa1f778f7b1ea5fb50ac826b38e1eb7268f6d708dcb1cd72811a30d2efc3cc`);
+			
+				let value = await s.json();
+				this.tickers.find(t => t.name == newTicker.name).price = value.USD;
+			}, 500);
 		},
 
 		tickerDelete(tickerKey) {
