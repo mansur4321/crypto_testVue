ec3a01d359ee4743a73bce800845a6dddde33abc
mansur4321 <jet_011@mail.ru>
2021-09-29 21:12:03 +0300

    add websoket ref. and  validation of invalid currencies
    Если не находит валюту в USD, ищет ее через BTC и переводит в USD. Если валюты не существует, то выделяет красным цветом.

--- cryptonomicon/src/api.js
+++ cryptonomicon/src/api.js
@@ -1,43 +1,130 @@
 let API_KEY_SOCKET = 'ef967394f6f8d36a024577928cd403fd5a15cc43e2cf3757642376e8e887c24a';
 const aggregateIndex = '5';
+const messageError = 'INVALID_SUB';
+
+const keyError = {
+	off: 1,
+	on: 0,
+}
+const name = {
+	BTC: 'BTC',
+	USD: 'USD',
+}
 
 let subscribedTickers = new Map();//[название, функция]
+let countIn = new Map();//[название, валюта в которой считаем]
 const socket = new WebSocket(`wss://streamer.cryptocompare.com/v2?api_key=${API_KEY_SOCKET}`);
+let priceBTC = 1;
 
-function websocketSubMessage(ticker) {
+
+function websocketSubMessage(ticker, countCurrensy) {
 	return JSON.stringify({
 	    "action" : "SubAdd",
-	    subs : [`5~CCCAGG~${ticker}~USD`],
+	    subs : [`5~CCCAGG~${ticker}~${countCurrensy}`],
 	})
 }
 
-function websocketUnsubMessage(ticker) {
+function websocketUnsubMessage(ticker, countCurrensy) {
 	return JSON.stringify({
 	    "action" : "SubRemove",
-	    subs : [`5~CCCAGG~${ticker}~USD`],
+	    subs : [`5~CCCAGG~${ticker}~${countCurrensy}`],
 	})
+}
+
+function nameInParameter(string, val) {
+	let nameTicker = string.slice(9);
+	return ridOfCurrency(ridOfCurrency(nameTicker, val), val)
+}
+
+function ridOfCurrency(nameTicker, val) {
+	return nameTicker.replace(`~${val}`, "");
+}
+
+function alternativeSubMethod(ticker) {
+
+	if (subscribedTickers.get(name.BTC) == undefined) {
+		socket.send(websocketSubMessage(name.BTC, name.USD));
+	}
+
+console.log(1);
+	socket.send(websocketSubMessage(ticker, name.BTC));
+}
+
+function subToCurrensyError(PARAMETER) {
+	const sub = subscribedTickers.get(nameInParameter(PARAMETER, name.BTC))
+	console.log(nameInParameter(PARAMETER, name.BTC));
+	let error = sub[1];
+console.log('???????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????');
+	error(nameInParameter(PARAMETER, name.BTC), keyError.on);
+
+	return
+}
+
+function subToCurrensy(FROMSYMBOL, PRICE) {
+	const sub = subscribedTickers.get(FROMSYMBOL);
+
+	let subscribed = sub[0];
+	let errorKey = sub[1];
+
+	subscribed(FROMSYMBOL, PRICE);
+	errorKey(FROMSYMBOL, keyError.off);
+
+	return
 }
 
 socket.addEventListener('message', (infoMess) => {
-	let { TYPE, FROMSYMBOL, PRICE} = JSON.parse(infoMess.data);
+	let {TYPE, FROMSYMBOL, PRICE, MESSAGE, PARAMETER, TOSYMBOL} = JSON.parse(infoMess.data);
+	console.log(infoMess.data);
 
+
+	if (FROMSYMBOL == name.BTC) {
+		priceBTC = PRICE;
+	}
+
+	if (MESSAGE == messageError) {
+
+		if (PARAMETER.indexOf('BTC~BTC') != -1){
+			subToCurrensyError(PARAMETER);
+
+			return
+		}
+
+
+		alternativeSubMethod(nameInParameter(PARAMETER, name.USD));
+
+		return
+	}
 
 	if (TYPE !== aggregateIndex) {
 		return
 	}
 
+	if(TOSYMBOL == name.BTC) {
+		PRICE = PRICE * priceBTC;
 
-	const sub = subscribedTickers.get(FROMSYMBOL);
-	sub(FROMSYMBOL, PRICE);
+		subToCurrensy(FROMSYMBOL, PRICE);
+		countIn.set(FROMSYMBOL, name.BTC);
+
+		return
+	}
+
+
+	subToCurrensy(FROMSYMBOL, PRICE);
 });
 
 
-export const subTicker = (tickerS, callback) => {
-	subscribedTickers.set(tickerS, callback);
-	socket.send(websocketSubMessage(tickerS));
+export const subTicker = (tickerS, cb_add) => {
+	subscribedTickers.set(tickerS, cb_add);
+	countIn.set(tickerS, name.USD);
+
+	socket.send(websocketSubMessage(tickerS, name.USD));
 }
 
 export const unsubTicker = tickerD => {
+	let countCurrensy = countIn.get(tickerD);
+
+	countIn.delete(tickerD);
 	subscribedTickers.delete(tickerD);
-	socket.send(websocketUnsubMessage(tickerD))
+
+	socket.send(websocketUnsubMessage(tickerD, countCurrensy));
 }
